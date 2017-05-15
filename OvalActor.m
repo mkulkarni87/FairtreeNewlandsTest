58f4444d1d0582c73d2843dcae6718e98d1143b4
mkulkarni87 <mayuresh87@gmail.com>
2017-05-15 15:24:12 +0200

    restoring older version

--- OvalActor.m
+++ OvalActor.m
@@ -1,13 +1,20 @@
-<!-- Author: Alexander Shvets (alex@githowto.com) -->
-<html>
-  <head>
-<<<<<<< HEAD
-    <link type="text/css" rel="stylesheet" media="all" href="style.css" />
-=======
-    <!-- no style -->
->>>>>>> master
-  </head>
-  <body>
-    <h1>Hello,World! Life is great!</h1>
-  </body>
-</html>
+sprintf('OvalActor')
+
+sprintf('Wednesday asdlkgjalrghjadfmorning edit...')
+
+sprintf('conflict clash')
+laslghalkfghalkgadlfgjkalgjadlgjka
+
+Another commit for stash check
+
+
+
+
+
+
+%The rain in Spain...
+%one two three
+%kf  change to seventeen
+
+%Keith was here AND My name is Mayuresh
+
